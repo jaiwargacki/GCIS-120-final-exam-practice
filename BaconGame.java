d12ab5fd8d53ba251a056cd3779507101f31835f
Jai Wargacki <jpw5681@rit.edu>
2022-04-26 07:15:03 -0400

    Update assignment
    Signed-off-by: Jai Wargacki <jpw5681@rit.edu>

---
+++ BaconGame.java
@@ -0,0 +1,32 @@
+package Practice1.solution;
+
+public class BaconGame {
+
+    public static void main(String[] args) {
+        /* Movie movie1 = new Movie("The Matrix");
+        Movie movie2 = new Movie("V for Vendetta");
+        Movie movie3 = new Movie("John Wick");
+
+        Actor actor1 = new Actor("Keanu Reeves");
+        Actor actor2 = new Actor("Carrie-Anne Moss");
+        Actor actor3 = new Actor("Hugo Weaving");
+        Actor actor4 = new Actor("Natalie Portman");
+        Actor actor5 = new Actor("Rupert Graves");
+        Actor actor6 = new Actor("Laurence Fishburne");
+        Actor actor7 = new Actor("Dean Winters");
+
+        movie1.addActor(actor1);
+        movie1.addActor(actor2);
+        movie1.addActor(actor3);
+        movie2.addActor(actor6);
+
+        movie2.addActor(actor3);
+        movie3.addActor(actor4);
+        movie3.addActor(actor5);
+
+        movie3.addActor(actor1);
+        movie3.addActor(actor6);
+        movie3.addActor(actor7); */
+    }
+    
+}
